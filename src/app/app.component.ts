a6569bdfd8592c1562168b41b1c326d2cd83fdab
Murillo <murillobilchesnavarro@gmail.com>
2019-09-07 11:01:36 -0300

    Implementando diretiva.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  photos = [
+    {
+      url:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/5/5a/Sultan_the_Barbary_Lion.jpg/440px-Sultan_the_Barbary_Lion.jpg',
+      description:'Leão'
+    },
+    {
+      url:
+      'https://www.worten.pt/i/0366c1e3c3f46a030081fde46d73d6ffff310aef.jpg',
+      description:'Leoa'
+    },
+    {
+      url:
+      'https://www.worten.pt/i/0366c1e3c3f46a030081fde46d73d6ffff310aef.jpg',
+      description:'Leoa'
+    }
+  ]
 }
