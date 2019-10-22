a22d46e4d2bf220e2f5875a512652b5bff202b88
Murillo Bilches Navarro <mnavarro@HYPERATIVA.NET>
2019-10-22 17:54:16 -0300

    Implementando resolvers.

---
+++ src/app/photos/photo-list/photo-list.resolver.ts
@@ -0,0 +1,18 @@
+import { Injectable } from "@angular/core";
+import { Resolve, ActivatedRouteSnapshot, RouterStateSnapshot } from "@angular/router";
+
+import { PhotoService } from "../photo.service";
+import { Photo } from "../photo/photo";
+import { Observable } from "rxjs";
+
+@Injectable({ providedIn: 'root'})
+export class PhotoListResolver implements Resolve<Observable<Photo[]>> {
+
+    constructor(private service: PhotoService) {}
+
+    resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+        const userName = route.params.userName;
+        return this.service.listFromUser(userName);
+    }
+
+}
