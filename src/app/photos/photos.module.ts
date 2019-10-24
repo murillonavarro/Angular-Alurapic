027b03c45bf0026838e884a561b77ff11ccded95
Murillo Bilches Navarro <mnavarro@HYPERATIVA.NET>
2019-10-24 10:25:55 -0300

    Implementando Submódulos.

--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -1,26 +1,13 @@
-import { HttpClientModule } from "@angular/common/http";
 import { NgModule } from "@angular/core";
-import { CommonModule } from "@angular/common";
-
-import { PhotoListComponent } from './photo-list/photo-list.component';
-import { PhotoFormComponent } from './photo-form/photo-form.component';
-import { PhotosComponent } from './photo-list/photos/photos.component';
-import { PhotoComponent } from "./photo/photo.component";
-import { filterByDescription } from "./photo-list/filter-by-description.pipe";
-import { LoadButtonComponent } from './photo-list/load-button/load-button.component';
+import { PhotoModule } from "./photo/photo.module";
+import { PhotoFormModule } from "./photo-form/photo-form.module";
+import { PhotoListModule } from "./photo-list/photo-list.module";
 
 @NgModule({
-    declarations: [ 
-        PhotoComponent,
-        PhotoListComponent,
-        PhotoFormComponent,
-        PhotosComponent,
-        filterByDescription,
-        LoadButtonComponent 
-    ],
     imports: [
-        HttpClientModule,
-        CommonModule
+        PhotoModule,
+        PhotoFormModule,
+        PhotoListModule
     ] 
 })
 export class PhotosModule {}
