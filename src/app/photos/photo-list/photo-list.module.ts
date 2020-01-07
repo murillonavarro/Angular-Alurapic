84440081110fe4808672511897bbcc13de2f726e
Murillo Bilches Navarro <mnavarro@HYPERATIVA.NET>
2020-01-07 09:44:58 -0300

    Componentizando o filtro.

--- src/app/photos/photo-list/photo-list.module.ts
+++ src/app/photos/photo-list/photo-list.module.ts
@@ -6,13 +6,15 @@ import { filterByDescription } from "./filter-by-description.pipe";
 import { CommonModule } from "@angular/common";
 import { PhotoModule } from "../photo/photo.module";
 import { CardModule } from "src/app/shared/components/card/card.module";
+import { SearchComponent } from "./search/search.component";
 
 @NgModule({
     declarations: [
         PhotoListComponent,
         PhotosComponent,
         LoadButtonComponent,
-        filterByDescription
+        filterByDescription,
+        SearchComponent
     ],
     imports: [ 
         CommonModule, 
